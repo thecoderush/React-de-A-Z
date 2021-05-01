6f4cd21067bc316d2b9cc80fef2eb69a07e32b07
thecoderush <pheanuk@gmail.com>
2021-05-01 14:34:46 +0200

    Set vscode emmet configuration and write first jsx in App.js

--- React-Learning/react-app/src/App.js
+++ React-Learning/react-app/src/App.js
@@ -1,9 +1,22 @@
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <h1>Hello World!</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <input type="text"/>
+      <input type="text"/>
+      <br/>
+      <button>Changer le state</button>
+      {true ? 1000 : "abc"}
     </div>
-  );
+
+  //  return React.createElement(
+  //   'div',
+  //   {className: 'App'},
+  //   React.createElement('h1', null, 'Hello World!'
+  //   )
+  //    )
+);
 }
 
 export default App;
