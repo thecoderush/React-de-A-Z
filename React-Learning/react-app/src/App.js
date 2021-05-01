c4cce4344068d61a38e990fed493b4b917864154
thecoderush <pheanuk@gmail.com>
2021-05-01 16:16:57 +0200

    Add button and onClick event in App.js

--- React-Learning/react-app/src/App.js
+++ React-Learning/react-app/src/App.js
@@ -4,17 +4,27 @@ import Title from "./Title.js";
 
 function App() {
 
-  
-  const [myState, setmyState] = useState("Hello react-app!")
+  console.log("Mise à jour!")
+
+  const initialState = "Hello react-app!"
+  const secondState = "20"
+  const [myState, setmyState] = useState(initialState)
   console.log(useState)
   console.log(useState("Hello react-app!"));
   console.log(myState);
+
+  let state = true;
+
+  const modifyState = () => {
+    setmyState(secondState)
+  }
 
   return (
     <div className="App">
       <Title />
       <Random />
-      {myState}
+      <h1>This is my state : {myState}</h1>
+      <button onClick={modifyState} >Change le state from App.js</button>
     </div>
   );
 }
