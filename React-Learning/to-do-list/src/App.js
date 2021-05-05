035c8d1bd771e106d3f34f185e8d16d579d026a2
thecoderush <pheanuk@gmail.com>
2021-05-05 09:14:39 +0200

    First commit. Add Bootstrop package. Create Components folder with Form and Item

--- React-Learning/to-do-list/src/App.js
+++ React-Learning/to-do-list/src/App.js
@@ -1,7 +1,16 @@
+import '..//node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Item from './Components/Item'
 
 function App() {
   return (
     <div className="App">
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <button className="btn btn-danger">Hello World!</button>
+      <h1 className="text-center mt-3">Todo-List</h1>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
     </div>
   );
 }
---
+++ React-Learning/to-do-list/src/Components/Item.js
@@ -0,0 +1,9 @@
+
+export default function Item() {
+    return (
+        <li className="border d-flex justify-content-between align-items-center p-2 m-2">
+            <div className="p-3">Txt</div>
+            <button className="btn btn-danger p-2 h-50">Supprimez</button>
+        </li>
+    )
+}
