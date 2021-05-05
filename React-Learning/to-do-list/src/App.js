07ef10c633e6b82fce6a6bbc126fe5ebe6dc4f47
thecoderush <pheanuk@gmail.com>
2021-05-05 10:46:21 +0200

    Initialize state in Form component with array of todos and pass it to his Item child component as props

--- React-Learning/to-do-list/src/App.js
+++ React-Learning/to-do-list/src/App.js
@@ -1,16 +1,14 @@
 import '..//node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Item from './Components/Item'
+import Form from './Components/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1>Lorem ipsum dolor sit amet.</h1>
-      <button className="btn btn-danger">Hello World!</button>
+
       <h1 className="text-center mt-3">Todo-List</h1>
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+ 
+      <Form />
     </div>
   );
 }
--- React-Learning/to-do-list/src/Components/Form.js
+++ React-Learning/to-do-list/src/Components/Form.js
@@ -2,6 +2,13 @@ import {useState} from 'react'
 import Item from './Item'
 
 export default function Form() {
+
+    const [dataArr, setDataArr] = useState([
+        {txt: "Promener le chien"},
+        {txt: "Sport"},
+        {txt: "Coder avec React"}
+    ])
+
     return (
         <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
             <form className="mb-3">
@@ -12,7 +19,11 @@ export default function Form() {
 
             <h2>Liste des choses à faire : </h2>
             <ul className="list-group">
-                <Item />
+                {dataArr.map((item, index) => {
+                    return (
+                        <Item txt={item.txt} key={index} />
+                    )
+                })}
             </ul>
         </div>
     )
