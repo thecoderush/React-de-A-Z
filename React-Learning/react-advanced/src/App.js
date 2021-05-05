7c4025bd704c1b2cbb4facf619d7aa693fd89e12
thecoderush <pheanuk@gmail.com>
2021-05-05 15:28:04 +0200

    Import hooks (useState, useEffect). Display state and change state with button onClick. Log in the browser console to test useEffect with array

--- React-Learning/react-advanced/src/App.js
+++ React-Learning/react-advanced/src/App.js
@@ -1,9 +1,22 @@
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  
+  const [dataComponent, setDataComponent] = useState(1)
+
+  useEffect(() => {
+    console.log("dataComponent changed")
+  }, [dataComponent])  
+
+  const changeState = () => {
+    setDataComponent(dataComponent+1)
+  }
+
   return (
     <div className="App">
-      Hello World!
+      <h1>Le state est {dataComponent}</h1>
+       <button onClick={changeState}>Increment state</button>
     </div>
   );
 }
