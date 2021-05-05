0271690246cd7082e2343ccaca99f80b5cb1db43
thecoderush <pheanuk@gmail.com>
2021-05-05 16:18:26 +0200

    Create a Timer with a new timer state and setInterval()

--- React-Learning/react-advanced/src/App.js
+++ React-Learning/react-advanced/src/App.js
@@ -27,6 +27,24 @@ function App() {
       })
   }, [])
 
+  const [timer, setTimer] = useState(1)
+
+  // setInterval(() => {
+  //   setTimer(timer+1)
+  // }, 1000) 
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      // setTimer(timer+1)
+      // console.log(timer)
+      setTimer(timer => timer+1)
+    }, 3000) 
+
+    return () => {
+      clearInterval(intervalID)
+    }
+  }, [])
+
   return (
     <div className="App">
       <h1>Le state est {dataComponent}</h1>
@@ -39,6 +57,7 @@ function App() {
           style={{width: "500px", marginTop: "50px"}}
         /> 
       }
+      <h1>Timer state : {timer}</h1>
     </div>
   );
 }
