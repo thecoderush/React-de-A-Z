5a46fb145b709defc24187d74385fb8ac280a44a
thecoderush <pheanuk@gmail.com>
2021-05-05 15:56:40 +0200

    Use fetch() to call Cat API and extract data for injection in the newly created state dataImg then display it with some style and from conditional rendering using &&

--- React-Learning/react-advanced/src/App.js
+++ React-Learning/react-advanced/src/App.js
@@ -13,10 +13,32 @@ function App() {
     setDataComponent(dataComponent+1)
   }
 
+  const [dataImg, setDataImg] = useState()
+
+  useEffect(() => {
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(data => {
+        console.log(data);
+        setDataImg(data[0].url)
+      })
+  }, [])
+
   return (
     <div className="App">
       <h1>Le state est {dataComponent}</h1>
-       <button onClick={changeState}>Increment state</button>
+      <button onClick={changeState}>Increment state</button>
+      <br/>
+      {dataImg && 
+        <img 
+          src={dataImg} 
+          alt="cat image" 
+          style={{width: "500px", marginTop: "50px"}}
+        /> 
+      }
     </div>
   );
 }
