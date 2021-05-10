f41e569294055f86cdcf8f8c3f9caa83be679212
thecoderush <pheanuk@gmail.com>
2021-05-10 20:23:20 +0200

    Add logic with short circuit operator for display or hide Modal component

--- React-Learning/react-components/src/Components/Modal/Modal.js
+++ React-Learning/react-components/src/Components/Modal/Modal.js
@@ -16,18 +16,20 @@ export default function Modal() {
                 className="btn-modal">OPEN
             </button>
 
-            <div className="overlay">
-                <div className="modal">
-                    <div className="modal-content">
-                        <h2>Hello Modal</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima cupiditate nostrum enim ipsum ut adipisci, voluptas ipsam vel quisquam a dolore magnam ullam doloribus. Odio vel maxime consectetur atque, iure corrupti voluptas veritatis similique labore, sapiente repellat consequuntur maiores perferendis est iste velit, modi reprehenderit rerum eveniet commodi omnis repudiandae!</p>
+            {modal && 
+                <div className="overlay">
+                    <div className="modal">
+                        <div className="modal-content">
+                            <h2>Hello Modal</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima cupiditate nostrum enim ipsum ut adipisci, voluptas ipsam vel quisquam a dolore magnam ullam doloribus. Odio vel maxime consectetur atque, iure corrupti voluptas veritatis similique labore, sapiente repellat consequuntur maiores perferendis est iste velit, modi reprehenderit rerum eveniet commodi omnis repudiandae!</p>
+                            <button
+                                onClick={toggleModal} 
+                                className="close-modal">CLOSE
+                            </button>
+                        </div>
                     </div>
-                    <button
-                        onClick={toggleModal} 
-                        className="close-modal">CLOSE
-                    </button>
                 </div>
-            </div>
+            }
         </>
     )
 }
